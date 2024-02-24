368e3b68b64ea525f26242b668ec660aab71106d
IvanPartsunev <ivan.parcunev@gmail.com>
2024-02-24 21:29:47 +0200

    Initial migration

---
+++ NaturalOriginContentOfCosmeticProducts/calculate_noi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CalculateNoiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NaturalOriginContentOfCosmeticProducts.calculate_noi'
---
+++ NaturalOriginContentOfCosmeticProducts/settings.py
@@ -0,0 +1,100 @@
+from decouple import config
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+
+    # django build in apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # third party apps
+
+    # project apps
+    'NaturalOriginContentOfCosmeticProducts.raw_materials',
+    'NaturalOriginContentOfCosmeticProducts.calculate_noi',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'NaturalOriginContentOfCosmeticProducts.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'NaturalOriginContentOfCosmeticProducts.wsgi.application'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "natural_origin_content_db",
+        "USER": "user_1",
+        "PASSWORD": "1111",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
