65126480d2dc2c88e26088c2a67c401676cd495f
IvanPartsunev <ivan.parcunev@gmail.com>
2024-04-10 01:35:21 +0300

    Adjusted styles, fixed raw material update select raw material

--- staticfiles/js/updateRawMaterial.js
+++
@@ -1,7 +0,0 @@
-// Responsible for changing raw materials to update in raw material update view
-
-document.querySelector("#select_material")
-    .addEventListener("change", (e) => {
-        const currentForm = e.currentTarget.parentElement
-        currentForm.submit()
-    });
